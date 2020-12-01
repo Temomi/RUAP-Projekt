6212fa63e50d973835d17c06aee9cf9677b52a3f
Student <student@iMac-1.local>
2020-12-01 18:28:43 +0100

    added hello

---
+++ hello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pozdrav iz aplikacije od Dominika Majdandzica");
+        }
+    }
+}
